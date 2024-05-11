ad71225f96bc4afb845e707c33c71c79a9e94570
fathursyh <fathur.syah1@gmail.com>
2024-05-11 10:51:05 +0700

    dynamic com still fail

--- src/components/header/NavigationBar.vue
+++ src/components/header/NavigationBar.vue
@@ -1,16 +1,42 @@
 <template>
-  <div class="header__navbar row justify-content-center align-items-center" style="width: 450px;">
+  <div
+    class="header__navbar row justify-content-center align-items-center"
+    style="width: 450px"
+  >
     <search-menu></search-menu>
-    <signup-menu></signup-menu>
+    <component :is="components[menuComponent]"></component>
   </div>
 </template>
-<script>
-  import SearchMenu from './SearchMenu.vue';
-  import SignupMenu from './SignupMenu.vue';
-  export default {
-    components: {
-      SearchMenu,
-      SignupMenu,
-    }
+<script setup>
+  import { ref, computed, watch } from "vue";
+  import { useStore } from "vuex";
+  import SearchMenu from "./SearchMenu.vue";
+  import SignupMenu from "./SignupMenu.vue";
+  import ProfileMenu from './ProfileMenu.vue'
+
+  const menuComponent = ref("signup-menu");
+  const store = useStore();
+
+  const components = {
+    "signup-menu": SignupMenu,
+    "profile-menu": ProfileMenu,
+  };
+
+  const getToken = computed(()=>{
+    return store.state.auth.token;
+  });
+
+  if(!getToken.value) {
+    menuComponent.value = 'signup-menu';
+  } else {
+    menuComponent.value = 'profile-menu';
   }
+
+  watch(getToken, (newValue, oldValue)=>{
+    if(!newValue.value) {
+      menuComponent.value = 'signup-menu';
+    } else {
+      menuComponent.value = 'profile-menu';
+    }
+  });
 </script>
