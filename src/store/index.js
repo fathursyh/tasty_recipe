f8144cb8b0684c5deb83b6d162961946f79dd268
fathursyh <fathur.syah1@gmail.com>
2024-05-17 15:27:28 +0700

    delete persisted

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import { createStore } from "vuex";
 import recipe from './recipe';
 import auth from './auth';
-import createPersistedState from 'vuex-persistedstate'
 
 export const store = createStore({
 
